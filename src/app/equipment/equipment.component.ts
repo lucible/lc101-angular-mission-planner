cb67437c15e959164ce8e21e969420ea7089846f
lucible <gobbldygook@letterboxes.org>
2019-12-18 18:15:03 -0600

    finished studio 3 part 1

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,11 +21,39 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   inactive: boolean[] = [false, false, false, false, false, false, false, false, false]
+   nearMaxMass: boolean = false
+   massRemaining: number = this.maximumAllowedMass - this.cargoMass
 
    constructor() { }
 
    ngOnInit() { }
 
    // Code your addItem function here:
+   addItem (item : object) : boolean {
+    if (this.cargoHold.length === this.maxItems) {
+      this.inactive[this.equipmentItems.indexOf(item)] = true
+    } else if (item["mass"] + this.cargoMass > this.maximumAllowedMass) {
+      this.inactive[this.equipmentItems.indexOf(item)] = true
+    } else {
+      this.cargoHold.push(item)
+      this.cargoMass = this.cargoMass + item["mass"]
+      this.massRemaining = this.maximumAllowedMass - this.cargoMass
+    }
+     
+    if (this.cargoMass > this.maximumAllowedMass + 200 || this.cargoMass < this.maximumAllowedMass - 200) {
+      return false
+    } else {
+      this.nearMaxMass = true
+      return true
+    }
+   }
+
+   emptyHold() {
+     this.cargoHold = []
+     this.cargoMass = 0
+     this.nearMaxMass = false
+     this.inactive = [false, false, false, false, false, false, false, false, false]
+   }
    
 }
